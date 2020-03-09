5b1de6ba1d10de9214e28b2a0190bef9a59d4efe
YichaoSun0813 <59580001+YichaoSun0813@users.noreply.github.com>
2020-03-08 17:10:31 -0700

    Update diners-list.component.js

--- src/components/diners-list.component.js
+++ src/components/diners-list.component.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import "./style.css"
 import axios from 'axios';
 import { Map } from 'google-maps-react';
 import { GoogleApiWrapper } from 'google-maps-react';
